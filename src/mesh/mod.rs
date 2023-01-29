d61ff57a165c4c71018395109973985b06cd8554
szabotudor <szabo.tudor@gmail.com>
2023-01-29 03:26:26 +0100

    Added a basic mesh struct

---
+++ src/mesh/mod.rs
@@ -0,0 +1,28 @@
+use super::video::*;
+use crate::video::backends;
+extern crate alloc;
+
+
+pub struct Mesh {
+    backend: *mut backends::VideoBackend,
+    backend_mesh_id: backends::MeshID
+}
+
+
+impl Mesh {
+    /// Create a new empty mesh compatible with the given backend\
+    /// 
+    /// `backend` -> The mesh will retain a pointer to the backend\
+    /// If this goes out of scope, drawing the mesh will fail, and the mesh must be created again
+    pub fn new(backend: *mut backends::VideoBackend) -> Self {
+        Mesh {
+            backend,
+            backend_mesh_id: 0
+        }
+    }
+
+    pub unsafe fn manual_load_vertices(&mut self, vertex_array_components_create_info: VertexArrayCreateInfo) {
+        self.backend_mesh_id = (*self.backend).new_mesh();
+        (*self.backend).mesh_data(self.backend_mesh_id, vertex_array_components_create_info);
+    }
+}
