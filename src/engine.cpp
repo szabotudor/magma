892a87eb2261dfbf108f7d5515499524b236e584
szabotudor <szabo.tudor@gmail.com>
2023-12-01 17:14:41 +0100

    Added simple fps window, and removed tests

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,7 +1,12 @@
 #include "engine.hpp"
 #include "file.hpp"
+#include "imgui.h"
+#include "imgui_impl_mgmgfx.h"
 #include "mgmgfx.hpp"
 #include "mgmwin.hpp"
+#include <bits/chrono.h>
+#include <chrono>
+#include <ratio>
 
 
 namespace mgm {
@@ -16,27 +21,38 @@ namespace mgm {
         graphics->connect_to_window(*window);
         graphics->clear_color(vec4f{0.1f, 0.2f, 0.3f, 1.0f});
 
-        static const vec3f verts[] = {vec3f{-0.5f, -0.5f, 0.0f}, vec3f{0.0f, 0.5f, 0.0f}, vec3f{0.5f, -0.5f, 0.0f}};
-        const auto mesh = graphics->make_mesh(
-            verts,
-            {}, {}, {}, 3,
-            nullptr, 0
-        );
-
-        graphics->make_shader_from_source(
-            "#version 460 core\n layout (location = 0) in vec3 a_pos; void main() { gl_Position = vec4(a_pos, 1.0f); }",
-            "#version 460 core\n out vec4 FragColor; void main() { FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f); }"
-        );
+        ImGui::SetCurrentContext(ImGui::CreateContext());
+        ImGui_ImplMgmGFX_Init(*graphics);
     }
 
     void MagmaEngineMainLoop::tick(float delta) {
         window->update();
+
+        ImGui_ImplMgmGFX_ProcessInput(*window);
+
+        ImGui::Begin("fps", nullptr,
+            ImGuiWindowFlags_NoBackground
+            | ImGuiWindowFlags_NoTitleBar
+            | ImGuiWindowFlags_NoSavedSettings
+            | ImGuiWindowFlags_NoResize
+            | ImGuiWindowFlags_AlwaysAutoResize
+            | ImGuiWindowFlags_NoMove
+            | ImGuiWindowFlags_NoMouseInputs
+        );
+        ImGui::SetWindowPos({0, 0});
+        ImGui::Text("FPS: %u", (uint32_t)(1.0f / delta));
+        ImGui::End();
+
+        const auto size = window->get_size();
+        static vec2u32 old_size{};
+        if (size.x() != old_size.x() || size.y() != old_size.y()) {
+			graphics->viewport(vec2i32{0, 0}, {static_cast<int>(size.x()), static_cast<int>(size.y())});
+            old_size = size;
+        }
     }
 
     void MagmaEngineMainLoop::draw() {
         graphics->clear();
-        graphics->draw(0, 0);
-        graphics->swap_buffers();
     }
 
     bool MagmaEngineMainLoop::running() {
@@ -44,8 +60,6 @@ namespace mgm {
     }
 
     MagmaEngineMainLoop::~MagmaEngineMainLoop() {
-        graphics->destroy_shader(0);
-        graphics->destroy_mesh(0);
         graphics->disconnect_from_window();
         delete graphics;
         delete window;
@@ -56,10 +70,29 @@ namespace mgm {
 int main(int argc, char** args) {
     mgm::MagmaEngineMainLoop magma{};
     
+    auto start = std::chrono::high_resolution_clock::now();
+    float avg_delta = 1.0f;
+    constexpr auto delta_avg_calc_ratio = 0.05f;
+
     bool run = true;
     while (run) {
-        magma.tick(1.0f);
+        ImGui_ImplMgmGFX_NewFrame();
+        ImGui::NewFrame();
+
+        const auto now = std::chrono::high_resolution_clock::now();
+        const auto delta = std::chrono::duration_cast<std::chrono::microseconds>(now - start).count();
+        start = now;
+
+        avg_delta = avg_delta * (1.0f - delta_avg_calc_ratio) + (float)delta * 0.001f * delta_avg_calc_ratio;
+
+        magma.tick((float)avg_delta * 0.001f);
         magma.draw();
+
+        ImGui::EndFrame();
+        ImGui::Render();
+        ImGui_ImplMgmGFX_RenderDrawData(ImGui::GetDrawData());
+        magma.graphics->swap_buffers();
+
         run = magma.running();
     }
 
