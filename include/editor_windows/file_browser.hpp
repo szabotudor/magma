aa9435264d2326801d0024897998198b1e75b79f
szabotudor <szabo.tudor@gmail.com>
2025-01-09 17:46:52 +0100

    File browser new file extension filter option in constructor

--- include/editor_windows/file_browser.hpp
+++ include/editor_windows/file_browser.hpp
@@ -13,12 +13,13 @@ namespace mgm {
         std::string file_extension = "";
         Path file_path = "project://";
         size_t selected_file = (size_t)-1;
+        std::function<void(const Path path)> callback{};
         bool allow_platform_paths = false;
+        bool only_good_extensions = false;
 
         std::vector<Path> folders_here{};
         std::vector<Path> files_here{};
 
-        std::function<void(const Path path)> callback{};
 
         enum class Mode {
             // Open an existing file
@@ -40,11 +41,12 @@ namespace mgm {
             std::string default_file_name = "New File";
             std::string default_file_extension = "";
             std::vector<uint8_t> default_file_contents{};
+            bool only_show_files_with_proper_extension = false;
         };
 
         FileBrowser(const Args& args)
-        : default_contents{args.default_file_contents}, file_name{args.default_file_name}, file_extension{args.default_file_extension}, allow_platform_paths{args.allow_paths_outside_project},
-        callback{args.callback}, mode{args.mode}, type{args.type} {
+        : default_contents{args.default_file_contents}, file_name{args.default_file_name}, file_extension{args.default_file_extension}, callback{args.callback},
+        allow_platform_paths{args.allow_paths_outside_project}, only_good_extensions{args.only_show_files_with_proper_extension}, mode{args.mode}, type{args.type} {
             window_name = "File Browser";
 
             folders_here = MagmaEngine{}.file_io().list_folders(file_path);
